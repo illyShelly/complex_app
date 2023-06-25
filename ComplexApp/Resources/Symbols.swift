8ae65d3d5032bf0f79e0e5bf3ac7807d8bb3ac78
Ilona Sellenberk <ilona.sell@gmail.com>
2023-06-25 22:25:42 +0200

    set up project, add symbols, colours

---
+++ ComplexApp/Resources/Symbols.swift
@@ -0,0 +1,16 @@
+//
+//  Symbols.swift
+//  ComplexApp
+//
+//  Created by Ilona Sellenberkova on 25/06/2023.
+//
+
+import SwiftUI
+
+enum Symbols {
+    static let person = Image(systemName: "person.2")
+    static let gear = Image(systemName: "gear")
+    static let plus = Image(systemName: "plus")
+}
+
+// because it's enum with static properties - cannot by accident create instance
---
+++ ComplexApp/Resources/Theme.swift
@@ -0,0 +1,16 @@
+//
+//  Theme.swift
+//  ComplexApp
+//
+//  Created by Ilona Sellenberkova on 25/06/2023.
+//
+
+import Foundation
+import SwiftUI
+
+enum Theme {
+    static let background = Color("background")
+    static let detailBackground = Color("detail-background")
+    static let text = Color("text")
+    static let bubble = Color("bubble")
+}
