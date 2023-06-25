61b69a6119b0f83b9d08e28674677c9cddd0d2f0
Ilona Sellenberk <ilona.sell@gmail.com>
2023-06-25 22:11:00 +0200

    Initial Commit

---
+++ ComplexApp/ComplexAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComplexAppApp.swift
+//  ComplexApp
+//
+//  Created by Ilona Sellenberkova on 25/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ComplexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
